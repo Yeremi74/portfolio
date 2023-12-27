ee485f819bee632c33243db6aaf1095e897a4f0e
Yeremi74 <yyeremi15@gmail.com>
2023-12-27 12:26:52 -0400

    fixed images proyect

--- utils.js
+++ utils.js
@@ -3,12 +3,12 @@ export const projectList = [
     title: 'E-Commerce',
     desc: ' Imagina tener una tienda online super amigable y fácil de usar, donde puedes buscar y filtrar productos de la manera que quieras. Ya sea que busques camisetas, pantalones o cualquier tipo de ropa, los encontrarás organizados en categorías. Y si tienes un producto específico en mente, solo escribe el nombre en la barra de búsqueda. ¿Lo mejor de todo? Todo esto es posible gracias a React.js, que hace que la página sea rápida y suave. Por otro lado, todos los productos que ves son añadidos desde Strapi, una herramienta en el back-end que nos permite personalizar cómo y qué productos se muestran',
     url: 'https://real-eyes-ecommerces.netlify.app/',
-    img: 'https://storied-crostata-698a39.netlify.app/projects/real eyes capture.png'
+    img: 'https://storied-crostata-698a39.netlify.app/real eyes capture.png'
   },
   {
     title: 'E-Commerce',
     desc: ' Imagina tener una tienda online super amigable y fácil de usar, donde puedes buscar y filtrar productos de la manera que quieras. Ya sea que busques camisetas, pantalones o cualquier tipo de ropa, los encontrarás organizados en categorías. Y si tienes un producto específico en mente, solo escribe el nombre en la barra de búsqueda. ¿Lo mejor de todo? Todo esto es posible gracias a React.js, que hace que la página sea rápida y suave. Por otro lado, todos los productos que ves son añadidos desde Strapi, una herramienta en el back-end que nos permite personalizar cómo y qué productos se muestran',
     url: 'https://real-eyes-ecommerces.netlify.app/',
-    img: 'https://storied-crostata-698a39.netlify.app/projects/real eyes capture.png'
+    img: 'https://storied-crostata-698a39.netlify.app/real eyes capture.png'
   }
 ]
