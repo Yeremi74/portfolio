d5ad5d1fd416cf6338d84e7a3fcf8f9bd365bc61
Yeremi74 <yyeremi15@gmail.com>
2024-05-13 17:51:15 -0400

    project ecommerceMern

--- utils.jsx
+++ utils.jsx
@@ -21,31 +21,49 @@ export const projectList = [
     title: 'E-Commerce MERN',
     desc: (
       <div>
-        Hemos creado una plataforma web que simplifica tanto la experiencia de
-        compra para los usuarios como la gestión para los administradores. Con
-        una vista de usuario intuitiva y un panel de administrador completo,
-        nuestra aplicación está diseñada para facilitar tanto la navegación como
-        la administración de tu tienda en línea.
+        <p>
+          He creado una plataforma web que simplifica tanto la experiencia de
+          compra para los usuarios como la gestión para los administradores. Con
+          una vista de usuario intuitiva y un panel de administrador completo,
+          nuestra aplicación está diseñada para facilitar tanto la navegación
+          como la administración de una tienda en línea.
+        </p>
         <br />
         <br />
         <strong>Experiencia del Usuario:</strong>
         <br />
-        La vista de usuario te permite explorar nuestra amplia variedad de
-        productos, categorías y colecciones de manera sencilla. Utiliza nuestros
-        filtros personalizables para ordenar los productos por precio, colección
-        o categoría, o simplemente utiliza nuestro buscador integrado para
-        encontrar lo que necesitas de forma rápida y precisa.
+        <p>
+          La vista de usuario te permite explorar la amplia variedad de
+          productos, categorías y colecciones de manera sencilla. Utiliza los
+          filtros personalizables para ordenar los productos por precio,
+          colección o categoría, o simplemente utiliza el buscador integrado
+          para encontrar lo que necesitas de forma rápida y precisa. Además,
+          puedes añadir productos al carrito de compra para una experiencia de
+          compra más conveniente.
+        </p>
         <br />
         <br />
         <strong>Panel de Administrador:</strong>
         <br />
-        Para los administradores, nuestro panel ofrece una gama completa de
-        herramientas para gestionar tu tienda en línea de manera eficiente.
-        Desde la creación y edición de productos, categorías y colecciones hasta
-        la gestión de usuarios registrados, todas las funciones necesarias están
-        al alcance de tu mano. Además, puedes visualizar datos importantes
-        mediante una variedad de gráficos para ayudarte a tomar decisiones
-        informadas sobre tu negocio.
+        <p>
+          Para los administradores, nuestro panel ofrece una gama completa de
+          herramientas para gestionar tu tienda en línea de manera eficiente.
+          Desde la creación y edición de productos, categorías y colecciones
+          hasta la gestión de usuarios registrados, todas las funciones
+          necesarias están al alcance de tu mano. Además, puedes visualizar
+          datos importantes mediante una variedad de gráficos para ayudarte a
+          tomar decisiones informadas sobre tu negocio.
+        </p>
+        <br />
+        <strong>
+          Para acceder al panel de administrador, ingresa con los siguiented
+          datos:
+        </strong>
+        <p>
+          <span className='user'>Email:</span>admin@gmail.com
+          <br />
+          <span className='user'>Contraseña:</span>admin1
+        </p>
       </div>
     ),
     url: 'https://frontendecommercemern.netlify.app/',
